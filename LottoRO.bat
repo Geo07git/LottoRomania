2b19c3391b4a36826703441fa9d3eea6dfaaeeb9
Georgeo07 <giany07@gmail.com>
2026-06-29 11:40:50 +0300

    update .csv

--- LottoRO.bat
+++ LottoRO.bat
@@ -4,4 +4,4 @@ cd /d %~dp0
 powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c npm run dev' -WindowStyle Hidden"
 
 timeout /t 2 >nul
-start http://localhost:3000
+start http://localhost:3001
